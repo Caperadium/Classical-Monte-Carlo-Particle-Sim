1d5f83c7a00bc512386e3c5133454b4ef65040c5
Caperadium <39166344+Caperadium@users.noreply.github.com>
2024-03-26 14:14:24 -0700

    Update MonteCarloSim.py

--- MonteCarloSim.py
+++ MonteCarloSim.py
@@ -1,9 +1,12 @@
 import numpy as np
-import csv
 import time
 import math
+import os
 
-
+'''
+This program simulates particle interactions based on given constants utilizing a monte-carlo satistical averaging method.
+Utilizes periodic boundary conditions for both the physical location and the energy calculation
+'''
 
 '''                
 Defining constants
@@ -29,7 +32,9 @@ def measure_PE(distance):
 Inputs:
     positions: Passes the positions array to the function
     chosen_particle_index: passes the chosen particles index to the function
-Measures the total energy of the system
+
+Measures the total energy of the system based on a chosen particle
+Also implements periodic boundary conditions for the energy.
 ''' 
 def measure_total_energy(positions, chosen_particle_index):
     energy = 0.0
@@ -38,7 +43,9 @@ def measure_total_energy(positions, chosen_particle_index):
         X_distance = positions[i][0] - positions[chosen_particle_index][0] 
         Y_distance = positions[i][1] - positions[chosen_particle_index][1]
 
-        if X_distance > L / 2:
+#Periodic boundary conditions for the energy below
+        
+        if X_distance > L / 2: 
             X_distance -= L
 
         if Y_distance > L / 2:
@@ -50,9 +57,13 @@ def measure_total_energy(positions, chosen_particle_index):
     return energy
 
 '''
-#calculates the probability of a move and decides whether to keep it based on if the energy of the system is lowered or increased. 
-#AKA Metropolis criterion. A move that lowers the energy of the system is automatically accepted. While a move that increases the energy
-#has a low probability of being accepted
+Inputs:
+    energy_before: energy before a particle move was made
+    energy_after: energy after a particle move was made
+
+calculates the probability of a move and decides whether to keep it based on if the energy of the system is lowered or increased. 
+AKA Metropolis criterion. A move that lowers the energy of the system is automatically accepted. While a move that increases the energy 
+has a low probability of being accepted
 '''
 def metropolis(energy_before, energy_after):
     delta_energy = energy_after - energy_before 
@@ -62,29 +73,32 @@ def metropolis(energy_before, energy_after):
         return False 
 
 '''
-Generates a random move within a range of -0.5 to 0.5 Angstroms of the particles current position. Move is in 2 dimensions.
+Generates a random move within a range of -0.5 to 0.5 Angstroms of a particles current position. Move is in 2 dimensions.
 '''
 def generate_move():
     move = np.random.uniform(-.5, .5, size=2)
     return move
 
 '''
+Inputs
+    positions_new: postitions of particles 
+
 This function enforces physical periodic boundary conditions
 It checks both the X and Y values of a specific particle and sees if it is outside of the dimensions of the box.
 If the particle is outside the allowable area it subtracts the length or height of box from the particles X or Y position 
 so that the particle appears on the exact opposite side of the box.
 '''
 
-def physical_periodic_boundary(positions_new):
+def physical_periodic_boundary(new_position):
 
     for i in range(2):
-        if positions_new[i] > L:
-            positions_new[i] -= L
+        if new_position[i] > L:
+            new_position[i] -= L
 
-        if positions_new[i] < 0:
-            positions_new[i] += L
+        if new_position[i] < 0:
+            new_position[i] += L
 
-    return positions_new
+    return new_position
 
 
 """
@@ -111,18 +125,19 @@ def Monte_Carlo(steps, specified_print_step):
                                                   
         positions_new = positions.copy()
 
-        energy_before = measure_total_energy(positions, chosen_particle_index)      #DONE iterate through particles not random, NOT DONEkeep array of N individual particle energies. one for before and one for after move
-                                                                                    #DONE calculate delta E. E old - E new. Use with metropolis criteria to decide if move is kept or not
-        move = generate_move()                                                                          #DONE Add to running total of energy of system
-                                                                                    #Start from crystalin state. Also try running at T = very low to get a crystaline structure to use.
-                                                                                    #Hexagonal close packing
+        energy_before = measure_total_energy(positions, chosen_particle_index)      
+                                                                                    
+        move = generate_move()                                                                          
+
+
         positions_new[chosen_particle_index] += move
 
-        positions_new[chosen_particle_index] = physical_periodic_boundary(positions_new[chosen_particle_index])     #Also starting from crystaline state where particles are closer than sigma
-                                                                                    #DONE Implement phyicsal and energy periodic boundary conditions
+        positions_new[chosen_particle_index] = physical_periodic_boundary(positions_new[chosen_particle_index])     
+                                                                                    
         energy_after = measure_total_energy(positions_new, chosen_particle_index)   
-        delta_energy = energy_after - energy_before                                 #don't take total energy initially, let run for a bit, equilibrate for a bit, then start running. During equilibrating don't store any data
-        if metropolis(energy_before, energy_after):                                 #implement cutoff distance
+        delta_energy = energy_after - energy_before    
+                                                                                     
+        if metropolis(energy_before, energy_after):                                 
             positions[chosen_particle_index] = positions_new[chosen_particle_index]
             delta_energy_cumulative += delta_energy
         chosen_particle_index += 1
@@ -138,17 +153,38 @@ def Monte_Carlo(steps, specified_print_step):
     print("Cumulative delta energy", delta_energy_cumulative)
 
 '''
-Copies position array into a csv file to be visualized using excel  
+Copies position array into a csv file to be visualized using excel. Generally saves it to user directory   
 '''
 def write_csv():
-    file_path = 'position_data.csv'
+    script_dir = os.path.dirname(__file__)
+    file_path = os.path.join(script_dir, 'position_data.csv')
 
-    with open(file_path, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(positions)
+    np.savetxt(file_path, positions, delimiter=',', header='X,Y')
     
 
-    
-Monte_Carlo(1000000, 10000)
+
+Monte_Carlo(10, 10000)
 write_csv()
-       
+
+'''  
+TO DO:
+Export positions as GIF
+    DONE iterate through particles not random, 
+keep array of N individual particle energies. one for before and one for after move
+    DONE calculate delta E. E old - E new. Use with metropolis criteria to decide if move is kept or not
+    DONE Add to running total of energy of system
+Start from crystalin state. Also try running at T = very low to get a crystaline structure to use.
+Hexagonal close packing
+Also starting from crystaline state where particles are closer than sigma
+DONE Implement phyisical and energy periodic boundary conditions         
+Don't take total energy initially, let run for a bit, equilibrate for a bit, then start running. During equilibrating don't store any data
+Implement cutoff distance
+'''           
+
+
+     
+
+
+
+
+
